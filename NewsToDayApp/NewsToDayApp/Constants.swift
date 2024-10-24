b2781fdd669cc22fb58101328087e6cc83115ff7
Dmitry Zyadik <dmitry.zyadik@gmail.com>
2024-10-24 11:10:02 +0500

    Add colors and Costrants.swift

---
+++ NewsToDayApp/NewsToDayApp/Constants.swift
@@ -0,0 +1,46 @@
+//
+//  Constants.swift
+//  NewsToDayApp
+//
+//  Created by Дмирий Зядик on 24.10.2024.
+//
+
+import Foundation
+
+
+struct K {
+    static let appName = "NewsToDayApp"
+    
+    struct Catagories {
+        
+        static let sports = "🏈 Sports"
+        static let politics = "⚖️ Politics"
+        static let life = "🌞 Life"
+        static let gaming = "🎮 Gaming"
+        static let animals = "🐻 Animals"
+        static let nature = "🌴 Nature"
+        static let food = "🍔 Food"
+        static let art = "🎨 Art"
+        static let history = "📜 History"
+        static let fashion = "👗 Fashion"
+        static let covid19 = "😷 Covid-19"
+        static let middleEast = "⚔️ Middle East"
+    }
+    
+    struct BrandColors {
+        static let purpleDark = "BrandPurpleDark"
+        static let purpleLight = "BrandPurpleLight"
+        static let purplePrimary = "BrandPurplePrimary"
+        static let purpleLighter = "BrandPurpleLighter"
+        
+        static let blackDark = "BrandBlackDark"
+        static let blackLight = "BrandBlackLight"
+        static let blackPrimary = "BrandBlackPrimary"
+        static let blackLighter = "BrandBlackLighter"
+        
+        static let greyDark = "BrandGreyDark"
+        static let greyLight = "BrandGreyLight"
+        static let greyPrimary = "BrandGreyPrimary"
+        static let greyLighter = "BrandGreyLighter"
+    }
+}
