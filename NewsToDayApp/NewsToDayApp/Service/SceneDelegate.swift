581f680e8e20688ae0c50f901c72c8568fdc46e8
My <uk472363@yanex.ru>
2024-10-26 14:59:21 +0300

    refactor SearchBar

--- NewsToDayApp/NewsToDayApp/Service/SceneDelegate.swift
+++ NewsToDayApp/NewsToDayApp/Service/SceneDelegate.swift
@@ -16,7 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: windowScene)
-        window.rootViewController = BookmarksViewController()
+        window.rootViewController = BrowseViewController()
 //        window.rootViewController = UINavigationController(rootViewController: TabBarViewController())
         
         window.makeKeyAndVisible()
