a00a90b8e8d635156ab17b4f7f55b91b542bf643
My <uk472363@yanex.ru>
2024-10-26 14:36:35 +0300

    Refactor SearchBar

--- NewsToDayApp/NewsToDayApp/Controllers/BrowseViewController.swift
+++ NewsToDayApp/NewsToDayApp/Controllers/BrowseViewController.swift
@@ -9,4 +9,54 @@ import UIKit
 
 class BrowseViewController: UIViewController {
     
+    private var articles: [Article] = []
+    private let networkManager = NetworkService.shared
+    private let navigationBar = CustomNavigationBar()
+    private let searchBar = SearchBar()
+    
+    
+    //MARK: - Life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        setupNavBar()
+        setupSearchBar()
+        setupConstraints()
+        
+    }
+    
+    //MARK: - Setup UI
+    private func setupNavBar() {
+        navigationBar.titleOfLabel.text = "Browse"
+        navigationBar.subTitleLabel.text = "Discover things of this world"
+        navigationBar.view.translatesAutoresizingMaskIntoConstraints = false
+        addChild(navigationBar)
+        view.addSubview(navigationBar.view)
+        navigationBar.didMove(toParent: self)
+    }
+    
+    private func setupSearchBar() {
+        searchBar.searchBar.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(searchBar.view)
+        
+    }
+    
+    //MARK: - Setup Constraints
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            navigationBar.view.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            navigationBar.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            navigationBar.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            navigationBar.view.heightAnchor.constraint(equalToConstant: 50),
+            
+            searchBar.view.topAnchor.constraint(equalTo: navigationBar.view.bottomAnchor, constant: 20),
+            searchBar.view.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
+            searchBar.view.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
+            
+        ])
+        
+    }
+    
 }
---
+++ NewsToDayApp/NewsToDayApp/CustomView/SearchBar.swift
@@ -0,0 +1,94 @@
+//
+//  SearchBar.swift
+//  NewsToDayApp
+//
+//  Created by user on 25.10.2024.
+//
+
+import UIKit
+
+class SearchBar: UIViewController, UISearchBarDelegate {
+    
+    let searchBar = UISearchBar()
+    private var articles: [Article] = []
+    private let networkManager = NetworkService.shared
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        
+        view.translatesAutoresizingMaskIntoConstraints = false
+        
+        setupSearchBar()
+        setupTextField()
+        setConstraints()
+        
+        
+    }
+    
+    private func setupSearchBar() {
+        searchBar.delegate = self
+        searchBar.searchTextField.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        searchBar.backgroundImage = UIImage()
+        searchBar.tintColor = .white
+        searchBar.barTintColor = .white
+        
+        view.addSubview(searchBar)
+    }
+    
+    private func setupTextField() {
+        if let searchBarTextField = searchBar.value(forKey: "searchField") as? UITextField {
+            searchBarTextField.backgroundColor = .systemGray6
+            searchBarTextField.textColor = .black
+            searchBarTextField.attributedPlaceholder = NSAttributedString(string: "Search", attributes: [.foregroundColor: UIColor.lightGray])
+            searchBarTextField.layer.cornerRadius = 12
+            if let glassIconView = searchBarTextField.leftView as? UIImageView {
+                glassIconView.tintColor = .darkGray
+            }
+        }
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            view.heightAnchor.constraint(equalToConstant: 56),
+            searchBar.topAnchor.constraint(equalTo: view.topAnchor),
+            searchBar.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            searchBar.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            searchBar.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            searchBar.searchTextField.topAnchor.constraint(equalTo: searchBar.topAnchor),
+            searchBar.searchTextField.leadingAnchor.constraint(equalTo: searchBar.leadingAnchor),
+            searchBar.searchTextField.trailingAnchor.constraint(equalTo: searchBar.trailingAnchor),
+            searchBar.searchTextField.bottomAnchor.constraint(equalTo: searchBar.bottomAnchor)
+            
+        ])
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        if let searchTerm = searchBar.text, !searchTerm.isEmpty {
+            loadArticle(searchTerm)
+        }
+    }
+    
+    private func loadArticle(_ text: String) {
+        
+        networkManager.fetchSearch(with: text) { [unowned self] result in
+            switch result {
+            case .success(let article):
+                self.articles = article
+                self.presentSearchScreen()
+            case .failure(let error):
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
+    private func presentSearchScreen() {
+        let searchResultVC = SearchViewController()
+        searchResultVC.articles = articles
+        searchResultVC.modalPresentationStyle = .formSheet
+        present(searchResultVC, animated: true, completion: nil)
+    }
+}
