beb5e17221b8ce84f227de6e4b7ac3155695583b
Suharik001 <suharev-a@list.ru>
2024-10-27 18:34:21 +0300

    Fix ArticleVC

--- NewsToDayApp/NewsToDayApp/Controllers/Article/ArticleViewController.swift
+++ NewsToDayApp/NewsToDayApp/Controllers/Article/ArticleViewController.swift
@@ -124,10 +124,11 @@ final class ArticleViewController: UIViewController {
         textView.isEditable = false
         textView.font = .systemFont(ofSize: 16, weight: .regular)
         textView.textColor = .black
+        textView.textContainerInset = UIEdgeInsets( top: 20, left: 20, bottom: 0, right: 20)
         textView.setContentHuggingPriority(.required, for: .vertical)
         textView.textAlignment = .justified
 //        textView.clipsToBounds = false
-        textView.showsVerticalScrollIndicator = false
+        //textView.showsVerticalScrollIndicator = false
         textView.translatesAutoresizingMaskIntoConstraints = false
         textView.text = article.content ?? "No content"
     }
@@ -183,8 +184,8 @@ final class ArticleViewController: UIViewController {
             labelAuthor.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             labelAuthor.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
 
-            textView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            textView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            textView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            textView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             textView.topAnchor.constraint(equalTo: imageView.bottomAnchor),
             textView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
             
