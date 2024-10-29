9f25d3587be470574b26e437f99ff16c32298697
Pavel Shirokiy <pshirokiy@gmail.com>
2024-10-30 01:49:07 +0300

    ready Base Profile

--- NewsToDayApp/NewsToDayApp/Controllers/ProfileViewController.swift
+++ NewsToDayApp/NewsToDayApp/Controllers/ProfileViewController.swift
@@ -22,11 +22,11 @@ class ProfileViewController: UIViewController {
         return fotoImage
     }()
     
-    private let userName: UITextField = {
-        let userName = UITextField()
+    private let userName: UILabel = {
+        let userName = UILabel()
+        userName.text = "Dev P"
         userName.font = .systemFont(ofSize: 16, weight: .semibold)
         userName.textColor = .black
-        userName.returnKeyType = .done
         userName.layer.cornerRadius = 12
         
         return userName
@@ -41,7 +41,29 @@ class ProfileViewController: UIViewController {
         return userEmail
     }()
     
+    private lazy var languageButton: UIButton = {
+        let languageButton = ProfileButtons(title: String(localized:"Language"), picName: "greaterthan")
+        languageButton.addTarget(self, action: #selector(languageTapped), for: .touchUpInside)
+        
+        return languageButton
+    }()
     
+    private lazy var termsButton: UIButton = {
+        let termsButton = ProfileButtons(title: String(localized:"Terms & Conditions"), picName: "greaterthan")
+        termsButton.addTarget(self, action: #selector(termsTapped), for: .touchUpInside)
+        
+        return termsButton
+    }()
+    
+    private lazy var signOutButton: UIButton = {
+        let signOutButton = ProfileButtons(title: String(localized:"Sign Out"), picName: "rectangle.portrait.and.arrow.forward")
+        signOutButton.addTarget(self, action: #selector(signOutTapped), for: .touchUpInside)
+        
+        return signOutButton
+    }()
+
+    
+   //MARK: - app load
     
     override func viewDidLoad() {
         view.backgroundColor = .white
@@ -57,14 +79,42 @@ class ProfileViewController: UIViewController {
         navigationBar.didMove(toParent: self)
     }
     
+//MARK: - buttons actions
+    
+    @objc func languageTapped(_ sender: UIButton) {
+        
+    }
+    
+    @objc func termsTapped(_ sender: UIButton) {
+        
+    }
+    
+    @objc func signOutTapped(_ sender: UIButton) {
+        let onBoardingView = OnboardingViewController()
+        
+        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+           let window = windowScene.windows.first {
+            window.rootViewController = onBoardingView
+            window.makeKeyAndVisible()
+            
+            UIView.transition(with: window, duration: 0.3, options: .transitionCrossDissolve, animations: nil, completion: nil)
+        }
+    }
+    
+ //MARK: - set constreints
+    
     private func setupConstreints() {
         view.addSubview(fotoImage)
         view.addSubview(userName)
         view.addSubview(userEmail)
+        view.addSubview(languageButton)
+        view.addSubview(termsButton)
+        view.addSubview(signOutButton)
+        view.subviews.forEach({$0.translatesAutoresizingMaskIntoConstraints = false})
         
-        fotoImage.translatesAutoresizingMaskIntoConstraints = false
-        userName.translatesAutoresizingMaskIntoConstraints = false
-        userEmail.translatesAutoresizingMaskIntoConstraints = false
+        //fotoImage.translatesAutoresizingMaskIntoConstraints = false
+        //userName.translatesAutoresizingMaskIntoConstraints = false
+        //userEmail.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             
@@ -84,8 +134,19 @@ class ProfileViewController: UIViewController {
             
             userEmail.topAnchor.constraint(equalTo: userName.bottomAnchor, constant: 8),
             userEmail.leadingAnchor.constraint(equalTo: userName.leadingAnchor),
-            userEmail.trailingAnchor.constraint(equalTo: userName.trailingAnchor)
+            userEmail.trailingAnchor.constraint(equalTo: userName.trailingAnchor),
             
+            languageButton.topAnchor.constraint(equalTo: fotoImage.bottomAnchor, constant: 44),
+            languageButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            languageButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.9),
+            
+            signOutButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -28),
+            signOutButton.centerXAnchor.constraint(equalTo: languageButton.centerXAnchor),
+            signOutButton.widthAnchor.constraint(equalTo: languageButton.widthAnchor),
+            
+            termsButton.bottomAnchor.constraint(equalTo: signOutButton.topAnchor, constant: -28),
+            termsButton.centerXAnchor.constraint(equalTo: signOutButton.centerXAnchor),
+            termsButton.widthAnchor.constraint(equalTo: signOutButton.widthAnchor)
         ])
     }
 }
